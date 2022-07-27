6b14b085c4d79ab55e143d22452059078fb89dfa
Adam Seidman <adamjseidman@gmail.com>
2022-07-27 17:51:04 -0400

    Add Basic Click Functionality

--- app/gartic.js
+++ app/gartic.js
@@ -1,3 +1,25 @@
+const numColors = 18
+const numBrushes = 5
+const numTools = 10
+
+let color = 0
+let brush = 0
+let tool = 0
+
 let garticKey = function (event) {
-    console.log(event.code)
+    switch (event.code) {
+        case "F16":
+            tool = (tool + 1) % numTools
+            document.getElementsByClassName("tool")[tool].click()
+            break
+        case "F17":
+            brush = (brush + 1) % numBrushes
+            document.getElementsByClassName("brush")[brush].click()
+            break
+        case "F18":
+            color = (color + 1) % numColors
+            document.getElementsByClassName("color")[color].click()
+            break
+        
+    }
 }
--- app/skribbl.js
+++ app/skribbl.js
@@ -1,3 +1,25 @@
+const numColors = 22
+const numBrushes = 4
+const numTools = 3
+
+let color = 0
+let brush = 0
+let tool = 0
+
 let skribblKey = function (event) {
-    console.log(event.code)
+    switch (event.code) {
+        case "F16":
+            tool = (tool + 1) % numTools
+            document.getElementsByClassName("tool")[tool].click()
+            break
+        case "F17":
+            brush = (brush + 1) % numBrushes
+            document.getElementsByClassName("brushSize")[brush].click()
+            break
+        case "F18":
+            color = (color + 1) % numColors
+            document.getElementsByClassName("colorItem")[color].click()
+            break
+        
+    }
 }
